0408c0ca7d2e350fe3e3e87c48142909dcdac38f
whc <772778995@qq.com>
2024-01-06 23:38:58 +0800

    feat: 修改返回类型

--- backend/src/types/response.middleware.d.ts
+++ backend/src/types/response.middleware.d.ts
@@ -20,10 +20,10 @@ type ResSchema<D = any> = {
 }
 
 type ResponseFn = {
-  <R extends ResSchema>(res: R): R['data']
-  return: <D extends any>(data: D) => D
-  success: (data: ResMsg) => void
-  info: (data: ResMsg) => void
-  warning: (data: ResMsg) => void
-  error: (data: ResMsg) => void
+  <R extends ResSchema>(res: R): R
+  return: <D extends any>(data: D) => ResSchema<D>
+  success: (data: ResMsg) => ResSchema<void>
+  info: (data: ResMsg) => ResSchema<void>
+  warning: (data: ResMsg) => ResSchema<void>
+  error: (data: ResMsg) => ResSchema<void>
 }
