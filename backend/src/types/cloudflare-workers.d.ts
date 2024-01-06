69d36f754febb8b874ae953b7b21f57315659a80
whc <772778995@qq.com>
2024-01-06 17:14:10 +0800

    docs: 添加自定义变量注释

--- backend/src/types/cloudflare-workers.d.ts
+++ backend/src/types/cloudflare-workers.d.ts
@@ -1,14 +1,25 @@
 declare interface Request {
+  /**
+   * 语言环境
+   * @default 'zh-CN'
+   */
   $locale: Locale
+
+  /** 翻译 */
   $t: Translate
 
+  /** 校验数据 */
   $v: Validation
 
+  /** 表单数据 */
   $body: <T = any>() => Promise<T>
 
+  /** 获取数据库 */
   $db: () => DB
 
+  /** 键值对存储 */
   $kv: KVNamespace
 
+  /** 响应 */
   $res: ResponseFn
 }
