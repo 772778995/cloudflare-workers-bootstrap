fb3216462b803e9e9d3cc1b91007dad7e7da23b2
whc <772778995@qq.com>
2024-01-06 17:18:54 +0800

    perf: 修改闭包函数

--- backend/src/middleware/db.middleware.ts
+++ backend/src/middleware/db.middleware.ts
@@ -3,11 +3,9 @@ import { drizzle } from 'drizzle-orm/d1'
 
 /** 数据库中间件 */
 export const dbMiddleware: any = async (req: IRequest, env: Env) => {
-  req.$db = (() => {
-    let db: DB
-    return () => {
-      if (!db) db = drizzle(env.DB)
-      return db
-    }
-  })()
+  let db: DB
+  req.$db = () => {
+    if (!db) db = drizzle(env.DB)
+    return db
+  }
 }
